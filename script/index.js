06150c1ec9b54a51b636e830b32bf8c05bf18399
Wardun-00 <wardun.bd@gmial.com>
2025-10-25 21:08:01 +0600

    added spinner loader

--- script/index.js
+++ script/index.js
@@ -5,6 +5,16 @@ const createElements =(arr) =>{
 };
 
 
+const manageSpinner = (status) => {
+  if(status == true){
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("word-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+}
+
 
 const loadLessons = () => {
     const url = "https://openapi.programming-hero.com/api/levels/all";
@@ -21,6 +31,7 @@ const removeActive = () =>{
 
 
 const loadLevelWord = (id) => {
+  manageSpinner(true)
  const url =`https://openapi.programming-hero.com/api/level/${id}`;
  fetch(url)
  .then(res => res.json())
@@ -85,6 +96,7 @@ const displayLevelWord = (words) => {
           </h2>
         </div>
         `;
+        manageSpinner(false)
         return;
     }
 
@@ -113,6 +125,7 @@ const displayLevelWord = (words) => {
         wordContainer.append(card)
     });
 
+    manageSpinner(false)
 }
 
 
